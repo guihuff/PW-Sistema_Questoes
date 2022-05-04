877b0b977bb3fac6f00c811debc00ad6f34069ec
GUIHUFF <ghuff@alunos.utfpr.edu.br>
2022-05-03 22:40:18 -0300

    My Dates, form init

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     $('#btn-menu-lateral').click(() => {
         $('#menu-lateral').slideToggle('slow', () => {});
-    });
-    
+        $('#container-questoes').toggleClass( 'col-lg-10 col-md-9 col-sm-8' );
+    });  
 });
